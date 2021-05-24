c3318d21f66c695c7d7c94277c1420cdab8498bb
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-05-24 10:24:33 +0530

    app component

---
+++ reactbasics/src/App-backup.js
@@ -0,0 +1,24 @@
+/*
+What is React Comp?
+How to write React Comp? 
+function component?
+class Comp?
+
+*/
+
+// Functional Comp
+//----------------------
+/* function App() {
+  return <h1> Hello GM</h1>;
+}
+export default App;
+ */
+
+/* let App = () => <h1>Hello, GM.......</h1>;
+export default App; */
+
+let App = () => {
+  return <h2> Hello, Good Morning....!</h2>;
+};
+
+export default App;
--- reactbasics/src/App.js
+++ reactbasics/src/App.js
@@ -1,5 +1,18 @@
+/*
+    1.Functional comp should return one and only element is JSX
+    2. export default <ComponetName>
+*/
+
+import React from "react";
 function App() {
-  return <h1> Hello, GM. App Component</h1>;
+  return (
+    <React.Fragment>
+      <h1> One </h1>
+      <h2>Two</h2>
+      <h3>Thre</h3>
+      <h4> Four</h4>
+    </React.Fragment>
+  );
 }
 
 export default App;
